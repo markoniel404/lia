f69f6948f62163ef86605075d4ad31535adff6f1
markoniel404 <jhawetjkasdfjkl@mailinator.com>
2014-12-10 14:21:41 -0500

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,53 @@
+inf = {
+	nextPage: -1,
+	nextUrl: false,
+	loading: false,
+	initialize: function() {
+		$('#threadnavigator_1 ul').append('<li><img src="http://i.imgur.com/kTTdW2l.gif" width="150px" height="150px" /></li>');
+		var inf = this;
+		$(window).bind("scroll", function(){
+			inf.updateScrollInfo();
+			if (inf.isNearBottom() && !inf.loading) {
+				inf.appendNextPage();
+			}
+		});
+	},
+	updateScrollInfo: function() {
+		this.contentHeight = $(document).height();
+		this.scrollPosition = $(document).scrollTop();
+		console.log(this.contentHeight + " : " + this.scrollPosition);
+	},
+	isNearBottom: function() {
+		return (this.contentHeight - this.scrollPosition) < 2000;
+	},
+	getNextUrl: function() {
+		if (!this.nextUrl) {
+			this.nextUrl = $("#pager .lia-paging-page-next a").attr('href').replace(/\d+$/g, '');
+			this.nextPage = Number($(".lia-component-pagesnumbered .lia-link-disabled").html()) + 1;
+		}
+		return this.nextUrl + this.nextPage++;
+	},
+	getNextPage: function(url, cb) {
+		var context = this;
+		$.get(url, function(data) { 
+				context.content = $.parseHTML(data); 
+				if (cb) {
+					cb(context);
+				}
+			} 
+		);
+	},
+	appendNextPage: function() {
+		if (!this.loading) {
+			this.loading = true;
+			this.getNextPage(this.getNextUrl(), function(context) {
+				if (context.content) {
+					$('.linear-message-list.message-list').append($('.linear-message-list.message-list', context.content));	
+					context.updateScrollInfo();
+					context.loading = false;
+				}
+			});
+		}
+	}
+}
+inf.initialize();
